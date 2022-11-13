cc97b3228314be20bca4d038a5b7875acde73aa4
Tomay0 <thomaspage33@gmail.com>
2022-11-13 19:08:00 +1300

    Create hello world lobby plugin

---
+++ src/main/java/beansplusplus/lobby/GameServer.java
@@ -0,0 +1,4 @@
+package beansplusplus.lobby;
+
+public class GameServer {
+}
---
+++ src/main/java/beansplusplus/lobby/LobbyPlugin.java
@@ -0,0 +1,16 @@
+package beansplusplus.lobby;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class LobbyPlugin extends Plugin {
+  @Override
+  public void onEnable() {
+    getLogger().info("Emabled LobbyPlugin");
+
+
+
+
+    // game create
+
+  }
+}
