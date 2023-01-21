112ed9ee6b4c327d82b8aa879fc86941fce5d419
Tomay0 <thomaspage33@gmail.com>
2023-01-21 20:20:49 +1300

    Game Manager code cleanup

--- src/main/java/beansplusplus/lobby/GameServer.java
+++ src/main/java/beansplusplus/lobby/GameServer.java
@@ -2,6 +2,7 @@ package beansplusplus.lobby;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.net.InetSocketAddress;
 
@@ -39,5 +40,9 @@ public class GameServer {
 
   public ServerInfo getServerInfo() {
     return ProxyServer.getInstance().getServerInfo(id);
+  }
+
+  public void connectPlayerToServer(ProxiedPlayer player) {
+    player.connect(getServerInfo());
   }
 }
